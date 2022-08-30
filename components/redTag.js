c506f91e1c5154c14fe932d8bb4da8d773194f07
Steven <steven@dominion79.co.uk>
2022-08-30 22:06:00 +0100

    Refactor to use colour tags

---
+++ components/redTag.js
@@ -0,0 +1,76 @@
+const cssStyles = {
+  input: 
+  [
+      'mt-1',
+      'focus:ring-indigo-500',  
+      'focus:border-indigo-500', 
+      'block w-full', 
+      'sm:text-sm', 
+      'bg-gray-50', 
+      'border-gray-200', 
+      'border-solid', 
+      'border-2',
+      'p-2', 
+      'rounded-md'
+  ],
+  suggestedAnswer: 
+  [   
+      'mt-1',
+      'h-96',
+      'focus:ring-indigo-500',  
+      'focus:border-indigo-500', 
+      'sm:text-sm', 
+      'bg-gray-50', 
+      'border-gray-200', 
+      'border-solid', 
+      'border-2',
+      'p-2', 
+      'rounded-md'
+  ],
+  button: 
+  [
+      'w-full', 
+      'bg-indigo-50', 
+      'border-indigo-200', 
+      'border-solid', 
+      'border-2',
+      'p-2',
+      'm-2',
+      'rounded-lg',
+      'hover:bg-indigo-200'
+  ],
+  redHighlight:
+  [
+      'bg-red-800', 
+      'text-white', 
+      'inline',
+      'text-sm', 
+      'p-1', 
+  ],
+  greenHighlight:
+  [
+      'bg-green-800', 
+      'text-white', 
+      'inline',
+      'text-sm', 
+      'p-1', 
+  ],
+  blueHighlight:
+  [
+      'bg-blue-800', 
+      'text-white', 
+      'inline',
+      'text-sm', 
+      'p-1', 
+  ]
+} 
+
+export default function RedTag({title}) {
+return (
+    <>
+      <span className={cssStyles.redHighlight.join(' ').toString()}>
+        {title}
+      </span>
+    </>
+)
+}
--- components/themes.js
+++ components/themes.js
@@ -1,3 +1,8 @@
+import { INTERNALS } from "next/dist/server/web/spec-extension/request";
+import RedTag from "./redTag";
+import BlueTag from "./blueTag";
+import GreenTag from "./greenTag";
+
 const cssStyles = {
     input: 
     [
@@ -65,52 +70,40 @@ const cssStyles = {
     ]
 } 
 
+const setThemeColour = (item) => { //TODO this is ugly 
+    switch (item.colour) {
+        case 'red': 
+            return <RedTag title={item.name}></RedTag>
+        case 'green':
+            return <GreenTag title={item.name}></GreenTag>
+        case 'blue':
+            return <BlueTag title={item.name}></BlueTag>
+        default:
+            return <RedTag title={item.name}></RedTag>
+    }
+}
+
 export default function Themes(selectedCaseStudy, setSelectedCaseStudy) {
-
-    const setThemeColour = (item) => { //TODO this is ugly 
-        switch (item) {
-            case 'red': 
-                return cssStyles.redHighlight.join(' ').toString()
-                break;
-            case 'green':
-                return cssStyles.greenHighlight.join(' ').toString()
-                break;
-            case 'blue':
-                return cssStyles.blueHighlight.join(' ').toString()
-                break;
-            default:
-                return cssStyles.redHighlight.join(' ').toString()
-                break;
-        }
+    if(selectedCaseStudy.selectedCaseStudy.themes) {
+        return (
+        <>
+        <div className="grid grid-cols-12">
+            <div className="col-span-1 my-5">
+                <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
+                Themes:
+                </label>
+            </div>
+            <div className="col-span-11 my-5">
+                <ul className="block list-disc text-black">
+                    {selectedCaseStudy.selectedCaseStudy.themes.map(item => (
+                        <li key={item.id} className="my-1">{setThemeColour(item)}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+        </>
+        )
     }
-
-    const listCaseStudyThemes = () => {
-        if(selectedCaseStudy.selectedCaseStudy.themes) {
-            return(
-                <>
-                <div className="col-span-1 my-5">
-                    <label htmlFor="first-name" className="block text-sm font-medium text-gray-700">
-                    Themes:
-                    </label>
-                </div>
-                <div className="col-span-11 my-5">
-                    <ul className="block list-disc text-black">
-                        {selectedCaseStudy.selectedCaseStudy.themes.map(item => (
-                            <li key={item.id} className="my-1"><span className={setThemeColour(item.colour)}>{item.name}</span></li>
-                        ))}
-                    </ul>
-                </div>
-                </>
-            )
-        }
-    }
-    return (
-    <>
-    <div className="grid grid-cols-12">
-        {listCaseStudyThemes()}
-    </div>
-    </>
-    )
 }
 
   
