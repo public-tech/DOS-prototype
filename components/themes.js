67d118fbcca64b2d790ce739489bc61ab97abdf8
Steven <steven@dominion79.co.uk>
2022-08-30 22:13:00 +0100

    Refactor themes to remove unused styles & imports

--- components/themes.js
+++ components/themes.js
@@ -1,74 +1,6 @@
-import { INTERNALS } from "next/dist/server/web/spec-extension/request";
 import RedTag from "./redTag";
 import BlueTag from "./blueTag";
 import GreenTag from "./greenTag";
-
-const cssStyles = {
-    input: 
-    [
-        'mt-1',
-        'focus:ring-indigo-500',  
-        'focus:border-indigo-500', 
-        'block w-full', 
-        'sm:text-sm', 
-        'bg-gray-50', 
-        'border-gray-200', 
-        'border-solid', 
-        'border-2',
-        'p-2', 
-        'rounded-md'
-    ],
-    suggestedAnswer: 
-    [   
-        'mt-1',
-        'h-96',
-        'focus:ring-indigo-500',  
-        'focus:border-indigo-500', 
-        'sm:text-sm', 
-        'bg-gray-50', 
-        'border-gray-200', 
-        'border-solid', 
-        'border-2',
-        'p-2', 
-        'rounded-md'
-    ],
-    button: 
-    [
-        'w-full', 
-        'bg-indigo-50', 
-        'border-indigo-200', 
-        'border-solid', 
-        'border-2',
-        'p-2',
-        'm-2',
-        'rounded-lg',
-        'hover:bg-indigo-200'
-    ],
-    redHighlight:
-    [
-        'bg-red-800', 
-        'text-white', 
-        'inline',
-        'text-sm', 
-        'p-1', 
-    ],
-    greenHighlight:
-    [
-        'bg-green-800', 
-        'text-white', 
-        'inline',
-        'text-sm', 
-        'p-1', 
-    ],
-    blueHighlight:
-    [
-        'bg-blue-800', 
-        'text-white', 
-        'inline',
-        'text-sm', 
-        'p-1', 
-    ]
-} 
 
 const setThemeColour = (item) => { //TODO this is ugly 
     switch (item.colour) {
@@ -82,6 +14,7 @@ const setThemeColour = (item) => { //TODO this is ugly
             return <RedTag title={item.name}></RedTag>
     }
 }
+
 
 export default function Themes(selectedCaseStudy, setSelectedCaseStudy) {
     if(selectedCaseStudy.selectedCaseStudy.themes) {
